f5861102c5d82ca778696ec62a1b499f8248cf0b
Jarv1sGH <itsmejarvis007@gmail.com>
2023-12-12 17:13:33 +0530

    UI Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
-import {SafeAreaView, Text, View, StatusBar} from 'react-native';
-import React from 'react';
-import {styles} from './Styles/main';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StatusBar,
+  FlatList,
+  Pressable,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import {styles} from './Styles/mainStyles';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faCopy} from '@fortawesome/free-solid-svg-icons';
+import {faPen} from '@fortawesome/free-solid-svg-icons';
+
 import Snackbar from 'react-native-snackbar';
+
 export default function App() {
+  const [isCircle, setCircle] = useState<boolean>(false);
+  const [winner, setWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const resetGame = () => {
+    setCircle(false);
+    setWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
+  const checkWinner = () => {};
   return (
     <SafeAreaView>
       <StatusBar />
-      <View>
-      {/* <FontAwesomeIcon size={22} icon={faCopy} /> */}
-        <Text>App</Text>
+      <View style={styles.mainContainer}>
+        <View>
+          {/* <FontAwesomeIcon size={22} icon={faCopy} /> */}
+          <Text style={styles.turnText}>
+            {isCircle ? `0's Turn` : ` X's Turn`}
+          </Text>
+          <FlatList
+            numColumns={3}
+            data={gameState}
+            style={styles.ticTacToeGrid}
+            renderItem={({item, index}) => (
+              <Pressable
+                key={index}
+                style={[
+                  styles.gameTile,
+                  // for rightmost column items with indexes (2,5,and 8) this will result in 2
+                  index % 3 === 2 ? styles.thirdColumn : null,
+                  // for bottom row items with indexes (6,7,and 8) this will result in 2
+                  Math.floor(index / 3) === 2 ? styles.thirdRow : null,
+                ]}
+                onPress={() => {}}>
+                <FontAwesomeIcon size={22} icon={faPen} />
+              </Pressable>
+            )}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              resetGame();
+            }}
+            style={styles.newGameBtn}>
+            <Text style={styles.newGameBtnText}>
+              {winner ? `Start New Game` : `Reset`}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
---
+++ src/Styles/mainStyles.tsx
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    height: '100%',
+    backgroundColor: '#29292B',
+  },
+
+  turnText: {
+    color: '#fff',
+    backgroundColor: 'tomato',
+    width: '90%',
+    borderRadius: 4,
+    textAlign: 'center',
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+    marginTop: 40,
+    fontSize: 20,
+  },
+  ticTacToeGrid: {
+    height: 360,
+    margin: 10,
+    borderRadius: 5,
+    marginTop: 30,
+  },
+  gameTile: {
+    width: '33.3%',
+    height: 120,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    borderRightWidth: 1,
+    borderRightColor: 'grey',
+    // margin: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  thirdColumn: {
+    borderRightWidth: 0,
+  },
+  thirdRow: {
+    borderBottomWidth: 0,
+  },
+  newGameBtn: {
+    backgroundColor: 'purple',
+    width: '90%',
+    borderRadius: 4,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+    marginTop: 30,
+  },
+  newGameBtnText: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
